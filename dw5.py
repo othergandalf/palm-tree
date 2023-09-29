719247bc937b42e18b68523817b6bfe2eea69848
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-09-29 13:53:51 -0400

    Update dw5.py

--- dw5.py
+++ dw5.py
@@ -25,6 +25,18 @@ mi_df = pd.DataFrame(mi_census)
 # county selection
 selected_county = st.selectbox('Select County', mi_df['NAME'])
 
+# Display selected county data
+st.write(f"### Commuting Data for {selected_county}")
+st.write(f"Total Commuters: {county_data['B08301_001E'].values[0]}")
+st.write(f"Driving Alone: {county_data['B08301_002E'].values[0]}")
+st.write(f"Carpooling: {county_data['B08301_003E'].values[0]}")
+st.write(f"Public Transportation: {county_data['B08301_008E'].values[0]}")
+st.write(f"Walking: {county_data['B08301_011E'].values[0]}")
+st.write(f"Cycling: {county_data['B08301_012E'].values[0]}")
+st.write(f"Taxicab, Motorcycle, or Other Means: {county_data['B08301_013E'].values[0]}")
+st.write(f"Worked from Home: {county_data['B08301_014E'].values[0]}")
+
+
 # Filter based on county
 county_data = mi_df[mi_df['NAME'] == selected_county]
 
