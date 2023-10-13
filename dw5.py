9d835e324bde74fc45cfd409a477d24d98b6caf6
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-10-13 12:28:48 -0400

    Update dw5.py

--- dw5.py
+++ dw5.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import geopandas as gpd
 import contextily as ctx
+import pydeck as pdk
 from census import Census
 from us import states
 
@@ -36,6 +37,30 @@ merged_data = michigan_counties.merge(mi_df, how='left', left_on='COUNTYFP', rig
 
 #contextily
 ctx.add_basemap(st.pydeck_chart(), crs="EPSG:3857")
+
+#pydeck
+deck_map = pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    initial_view_state=pdk.ViewState(
+        latitude= 42.2,
+        longitude=84.4,
+        zoom=6,
+    ),
+    layers=[
+        # Add your data layers here if needed
+    ],
+)
+
+# Add basemap (ctx)
+bmp = ctx.providers.CartoDB.PositronNoLabels  # You can choose a different basemap if you prefer
+deck_map = deck_map.to_pydeck()
+deck_map = deck_map.add_basemap(bmp)
+
+# Display the map using Streamlit
+st.pydeck_chart(deck_map)
+# Replace YOUR_LATITUDE, YOUR_LONGITUDE, and YOUR_ZOOM_LEVEL with the appropriate values for your map's initial view state. Also, modify the layers parameter in the Deck() function to add any data layers you want to display on the map.
+
+
 
 #county selection
 selected_county = st.selectbox('Select County', mi_df['NAME'])
