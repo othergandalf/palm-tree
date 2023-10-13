ca18fdcda1ccbf3a3309478447b089f71795e040
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-10-13 18:49:54 -0400

    Update dw5.py

--- dw5.py
+++ dw5.py
@@ -16,22 +16,6 @@ michigan_counties = gpd.read_file(michigan_counties_url)
 # API key
 c = Census("2cad02e99c0bde70c790f7391ffb3363c5e426ef")
 
-import streamlit as st
-import pandas as pd
-import geopandas as gpd
-from census import Census
-from us import states
-
-st.title('Michigan Commuting Data')
-
-# Import .shp file
-michigan_counties_url = "https://www2.census.gov/geo/tiger/TIGER_RD18/STATE/26_MICHIGAN/26/tl_rd22_26_cousub.zip"
-# Load the Shapefile directly from the .zip file
-michigan_counties = gpd.read_file(michigan_counties_url)
-
-# API key
-c = Census("2cad02e99c0bde70c790f7391ffb3363c5e426ef")
-
 mi_census = c.acs5.state_county(fields=('NAME',
                                         'B08301_001E',
                                         'B08301_002E',
