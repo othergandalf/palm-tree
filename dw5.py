344cf3d8590b0fa4cc54e010d74964f5097210b0
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-10-13 19:50:05 -0400

    Update dw5.py

--- dw5.py
+++ dw5.py
@@ -1,63 +1,31 @@
-import streamlit as st
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+counties["features"][0]
+
 import pandas as pd
-import geopandas as gpd
-import contextily as ctx
-from census import Census
-from us import states
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
+df.head()
 
-st.title('Michigan Commuting Data')
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
 
-# Import .shp file
-michigan_counties_url = "https://www2.census.gov/geo/tiger/TIGER_RD18/STATE/26_MICHIGAN/26/tl_rd22_26_cousub.zip"
-# Load the Shapefile directly from the .zip file
-michigan_counties = gpd.read_file(michigan_counties_url)
+import pandas as pd
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
 
-# API key
-c = Census("2cad02e99c0bde70c790f7391ffb3363c5e426ef")
+import plotly.express as px
 
-mi_census = c.acs5.state_county(fields=('NAME',
-                                        'B08301_001E',
-                                        'B08301_002E',
-                                        'B08301_003E',
-                                        'B08301_008E',
-                                        'B08301_011E',
-                                        'B08301_012E',
-                                        'B08301_013E',
-                                        'B08301_014E'),
-                               state_fips=states.MI.fips,
-                               county_fips="*",
-                               year=2021)
-
-mi_df = pd.DataFrame(mi_census)
-
-# Merge DataFrames
-merged_data = michigan_counties.merge(mi_df, how='left', left_on='COUNTYFP', right_on='county')
-
-# Select specific columns for visualization
-columns_to_visualize = ['B08301_002E', 'B08301_003E', 'B08301_008E', 'B08301_011E', 'B08301_012E', 'B08301_013E', 'B08301_014E']
-
-# County selection
-selected_county = st.selectbox('Select County', mi_df['NAME'])
-
-# Filter data based on county selection
-county_data = merged_data[merged_data['NAME'] == selected_county]
-
-# Display bar chart for the selected county
-st.bar_chart(county_data[columns_to_visualize])
-
-# Display map with the merged data and basemap
-with st.expander("Show Map"):
-    # Convert DataFrame to GeoDataFrame
-    county_data_gdf = gpd.GeoDataFrame(county_data, geometry='geometry')
-
-    # Set up basemap
-    basemap = ctx.providers.Stamen.TerrainBackground
-
-    # Plot GeoDataFrame with basemap using contextily
-    ax = county_data_gdf.plot(figsize=(12, 12), alpha=0.7, cmap='coolwarm', legend=True)
-    ctx.add_basemap(ax, crs=county_data_gdf.crs, source=basemap)
-
-    # Show legend
-    st.pyplot()
-
-# You can add more interactivity and visualization options as needed
+fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           scope="usa",
+                           labels={'unemp':'unemployment rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
