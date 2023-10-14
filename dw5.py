2db728c347a3b08fe8e4d2f80e9e972812a4dc00
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-10-14 12:02:15 -0400

    Update dw5.py

--- dw5.py
+++ dw5.py
@@ -4,11 +4,11 @@ import geopandas as gpd
 import plotly.express as px
 from census import Census
 from us import states
-
-
+# TITLE
 st.title('Michigan Commuting Data')
-
+# KEY
 c = Census("2cad02e99c0bde70c790f7391ffb3363c5e426ef")
+
 mi_census = c.acs5.state_county(fields=('NAME',
                                         'B08301_001E',
                                         'B08301_002E',
@@ -21,19 +21,19 @@ mi_census = c.acs5.state_county(fields=('NAME',
                                state_fips=states.MI.fips,
                                county_fips="*",
                                year=2021)
-
+#DF 
 mi_df = pd.DataFrame(mi_census)
 
 mi_df.head()
-# county selection
+# BOX 1
 selected_county = st.selectbox('Select County', mi_df['NAME'])
 
 st.markdown("Select a MI county to see the mix of commuting types for that county.")
 
-# Filter based on county
+# SELECT BOX 1
 county_data = mi_df[mi_df['NAME'] == selected_county]
 
-# Mapping clean names to variable codes
+# NEW CLEAN NAMES
 variable_names = {
     'B08301_002E': 'Driving Alone',
     'B08301_003E': 'Carpooling',
@@ -44,31 +44,42 @@ variable_names = {
     'B08301_014E': 'Worked from Home'
 }
 
-# Create a new DataFrame with clean variable names
+# NEW DF
 clean_data = county_data.rename(columns=variable_names)
 
-# Display selected county data with clean variable names
+# TEXT DISPLAY BOXES OF COUNTY SELECTED
 st.write(f"### Commuting Data for {selected_county}")
 st.write(f"Total Commuters: {county_data['B08301_001E'].values[0]}")
 for variable, name in variable_names.items():
     st.write(f"{name}: {county_data[variable].values[0]}")
 
-# Bar chart for the selected county with clean variable names
-st.bar_chart(clean_data[['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home']])
+# BAR CHART
+st.bar_chart(clean_data[['Driving Alone',
+                         'Carpooling',
+                         'Public Transportation',
+                         'Walking', 'Cycling', 
+                         'Other Means',
+                         'Worked from Home']])
 
 #                   -  PLOTLY  -
-# Load shapefile
+# CENSUS SHAPEFILE
 shapefile_path = "https://www2.census.gov/geo/tiger/TIGER_RD18/STATE/26_MICHIGAN/26/tl_rd22_26_cousub.zip"
 gdf = gpd.read_file(shapefile_path)
 
-# Merge census data with shapefile
+# MERGE
 merged_df = gdf.merge(mi_df, how='left', left_on='COUNTYFP', right_on='county')
 
 st.markdown("Below is an interactive map of a commuting type, and the counties that effects. These are estimates, and are meant to be intepreted as such: more rural counties are subject to higher error.")
-# Variable selection
-selected_variable = st.selectbox('Select Variable', ['B08301_002E', 'B08301_003E', 'B08301_008E', 'B08301_011E', 'B08301_012E', 'B08301_013E', 'B08301_014E'])
+# SELECT BOX 2
+selected_variable = st.selectbox('Select Variable', ['B08301_002E',
+                                                     'B08301_003E',
+                                                     'B08301_008E', 
+                                                     'B08301_011E',
+                                                     'B08301_012E', 
+                                                     'B08301_013E', 
+                                                     'B08301_014E'])
 
-# Plotly Choropleth Map
+# CHLOROPLETH PX MAP 
 fig = px.choropleth(merged_df, 
                     geojson=merged_df.geometry, 
                     locations=merged_df.index, 
