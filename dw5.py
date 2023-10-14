283d1a1445b1d7f2b0f6434f91b524d5489a9af3
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-10-13 22:05:38 -0400

    Update dw5.py

--- dw5.py
+++ dw5.py
@@ -39,9 +39,28 @@ st.write(f"Cycling: {county_data['B08301_012E'].values[0]}")
 st.write(f"Taxicab, Motorcycle, or Other Means: {county_data['B08301_013E'].values[0]}")
 st.write(f"Worked from Home: {county_data['B08301_014E'].values[0]}")
 
-# bar chart for the selected county
-st.bar_chart(county_data[['B08301_002E', 'B08301_003E', 'B08301_008E', 'B08301_011E', 'B08301_012E', 'B08301_013E', 'B08301_014E']])
+# Mapping clean names to variable codes
+variable_names = {
+    'B08301_002E': 'Driving Alone',
+    'B08301_003E': 'Carpooling',
+    'B08301_008E': 'Public Transportation',
+    'B08301_011E': 'Walking',
+    'B08301_012E': 'Cycling',
+    'B08301_013E': 'Other Means',
+    'B08301_014E': 'Worked from Home'
+}
 
+# Create a new DataFrame with clean variable names
+clean_data = county_data.rename(columns=variable_names)
+
+# Display selected county data with clean variable names
+st.write(f"### Commuting Data for {selected_county}")
+st.write(f"Total Commuters: {county_data['B08301_001E'].values[0]}")
+for variable, name in variable_names.items():
+    st.write(f"{name}: {county_data[variable].values[0]}")
+
+# Bar chart for the selected county with clean variable names
+st.bar_chart(clean_data[['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home']])
 
 
 
