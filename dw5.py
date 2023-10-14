2e2ad1bf79d8ccb2675f44d643eb4aaa28d12908
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-10-13 22:38:19 -0400

    Update dw5.py

--- dw5.py
+++ dw5.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
+import plotly.express as px
 from census import Census
 from us import states
+
 
 st.title('Michigan Commuting Data')
 
@@ -25,7 +28,7 @@ mi_df.head()
 # county selection
 selected_county = st.selectbox('Select County', mi_df['NAME'])
 
-st.text("test")
+st.text("Select a MI county to see the mix of commuting types for that county.")
 
 # Filter based on county
 county_data = mi_df[mi_df['NAME'] == selected_county]
@@ -53,9 +56,28 @@ for variable, name in variable_names.items():
 # Bar chart for the selected county with clean variable names
 st.bar_chart(clean_data[['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home']])
 
+#                   -  PLOTLY  -
+# Load shapefile
+shapefile_path = "https://www2.census.gov/geo/tiger/TIGER_RD18/STATE/26_MICHIGAN/26/tl_rd22_26_cousub.zip"
+gdf = gpd.read_file(shapefile_path)
+
+# Merge census data with shapefile
+merged_df = gdf.merge(mi_df, how='left', left_on='county', right_on='COUNTYFP')
 
 
+# Variable selection
+selected_variable = st.selectbox('Select Variable', ['B08301_002E', 'B08301_003E', 'B08301_008E', 'B08301_011E', 'B08301_012E', 'B08301_013E', 'B08301_014E'])
 
+# Plotly Choropleth Map
+fig = px.choropleth(merged_df, 
+                    geojson=merged_df.geometry, 
+                    locations=merged_df.index, 
+                    color=selected_variable,  # Variable selected by the user
+                    hover_name='NAME',
+                    color_continuous_scale='Viridis',
+                    range_color=(0, merged_df[selected_variable].max()))  # Adjust the color scale based on the selected variable
+
+st.plotly_chart(fig)
 
 
 
