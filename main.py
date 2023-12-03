b7bb126c851013da57b80bd4543aa760863b46b6
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-03 16:27:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # main.py
 import streamlit as st
-import page1
-import page2
+import cenreg
+import commuting
 
 st.set_page_config(page_title='Michigan Commuting Data', page_icon='🚗')
 
 page = st.sidebar.radio("Select Page", ["Page 1", "Page 2"])
 
-if page == "Page 1":
-    page1.show()
-elif page == "Page 2":
-    page2.show()
+if page == "Commuting Data Map":
+    commuting.show()
+elif page == "KNN Model":
+    cenreg.show()
