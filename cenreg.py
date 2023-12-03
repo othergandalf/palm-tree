be55f3284135ebfdd16c29a86db936dd421c0108
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-03 16:59:54 -0500

    Update cenreg.py

--- cenreg.py
+++ cenreg.py
@@ -1,4 +1,3 @@
-def show():
 import streamlit as st
 import pandas as pd
 import geopandas as gpd
@@ -6,6 +5,7 @@ import pydeck as pdk
 import plotly.express as px
 from census import Census
 from us import states
+def show():
 # TITLE
   st.title('Michigan Commuting Data')
 # KEY
