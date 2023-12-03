e11837e5fedce81bcb7af4fb142ec3fde0c7e6c0
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-03 16:49:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ st.set_page_config(page_title='Michigan Commuting Data', page_icon='🚗')
 page = st.sidebar.radio("Select Page", ["Commuting Data Map", "KNN Model"])
 
 if page == "Commuting Data Map":
-    commuting.show()
+commuting.show()
 elif page == "KNN Model":
-    cenreg.show()
+cenreg.show()
