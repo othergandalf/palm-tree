fdcee58af81d1f87bcba7e245469bc360883b707
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-03 19:52:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ st.set_page_config(page_title='Michigan Commuting Data', page_icon='🚗')
 page = st.sidebar.radio("Select Page", ["Exploring County-level Data", "Modeling Tract-level Data"])
 
 if page == "Exploring County-level Data":
-  commuting.show()
+commuting.show()
 elif page == "Modeling Tract-level Data":
-  cenreg.show()
+cenreg.show()
