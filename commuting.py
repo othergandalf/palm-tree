98835589ae42e82f5748e344f0a206ba2c689415
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-03 20:32:23 -0500

    Update commuting.py

--- commuting.py
+++ commuting.py
@@ -6,8 +6,8 @@ def show():
   import plotly.express as px
 from census import Census
 from us import state
-  # TITLE
- st.title('Michigan Commuting Data')
+# TITLE
+st.title('Michigan Commuting Data')
 # KEY
 c = Census("2cad02e99c0bde70c790f7391ffb3363c5e426ef")
 
