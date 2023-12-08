815e1dfbbaf4cae214e6070dab76897c69f54ed5
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-08 15:57:04 -0500

    Update cenreg.py

--- cenreg.py
+++ cenreg.py
@@ -7,7 +7,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 import plotly.express as px
 
-@st.cache(allow_output_mutation=True)
 def fetch_census_data():
     c = Census("2cad02e99c0bde70c790f7391ffb3363c5e426ef")
     fields = [
@@ -44,7 +43,6 @@ def fetch_census_data():
 
     return df
 
-@st.cache(allow_output_mutation=True)
 def train_knn_model(df, y_variable):
     selected_features = ['Total Population', 'Median Income', 'Poverty Rate', 'Time of Commute']
     X = df[selected_features]
@@ -62,24 +60,58 @@ def train_knn_model(df, y_variable):
 
     return knn_model, scaler
 
-@st.cache
 def make_predictions(knn_model, scaler, user_input):
     scaled_input = scaler.transform([user_input])
     prediction = knn_model.predict(scaled_input)
 
-    return prediction[0]
+    return prediction[0]  # Return the first (and only) element of the prediction array
 
 def show():
-    # ... (same as before)
+    st.title('KNN Model Page')
 
-    if update_button:
-        # Make predictions using the copied dataframe
-        prediction = make_predictions(knn_model, scaler, user_input)
-        st.write(f"Updated Prediction ({knn_y_variable}): {prediction}")
+    # Load data
+    df = fetch_census_data()
 
-    # ... (same as before)
+    # KNN model training
+    st.header('KNN Model Training')
 
-# Run the app
-show()
+    # User selects the y-variable for the commute
+    y_variable = st.selectbox("Select Commute Variable for KNN Model", ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home'],
+                              key="0001")
+
+    # Train the KNN model and get the scaler based on user-selected y-variable
+    knn_model, scaler = train_knn_model(df, y_variable)
+
+    # Add widgets for user inputs with unique keys
+    total_population_slider = st.slider("Total Population", key="total_population", min_value=0, max_value=10000, value=5000)
+    median_income_slider = st.slider("Median Income", key="median_income", min_value=0, max_value=100000, value=50000)
+    poverty_rate_slider = st.slider("Poverty Rate", key="poverty_rate", min_value=0, max_value=100, value=10)
+    time_of_commute_slider = st.slider("Time of Commute (minutes)", key="time_of_commute", min_value=0, max_value=120, value=30)
+
+    # User inputs
+    user_input = [total_population_slider, median_income_slider, poverty_rate_slider, time_of_commute_slider]
+
+    # Make predictions
+    prediction = make_predictions(knn_model, scaler, user_input)
+    st.write(f"Updated Prediction ({y_variable}): {prediction}")
+
+    # Plotting the data using Plotly Express with user customization
+    st.header('Commute Count at the Tract-Level')
+    color_variable = 'Poverty Rate'  # Assuming this as a default color variable
+    graph_y_variable = st.selectbox("Select Y-Axis Commute Variable in Scatterplot",
+                                     ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home'],
+                                     key="0002")
+
+
+    # Tract visual
+    fig = px.scatter(df,
+        x='Median Income',
+        y=graph_y_variable,
+        color=color_variable,
+        size='Total Population',
+        hover_data=['NAME']
+    )
+
+    st.plotly_chart(fig)
 
 show()
