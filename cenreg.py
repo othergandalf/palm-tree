fdfcd6873fdcc2666d4cd1345b98564f0c2035a1
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-07 21:27:36 -0500

    Update cenreg.py

--- cenreg.py
+++ cenreg.py
@@ -79,19 +79,19 @@ def show():
     knn_model, scaler = train_knn_model(df)
 
   # Add widgets for user inputs
-total_population_slider = st.slider("Total Population", min_value=0, max_value=10000, value=5000)
-median_income_slider = st.slider("Median Income", min_value=0, max_value=100000, value=50000)
-poverty_rate_slider = st.slider("Poverty Rate", min_value=0, max_value=100, value=10)
-time_of_commute_slider = st.slider("Time of Commute (minutes)", min_value=0, max_value=120, value=30)
+    total_population_slider = st.slider("Total Population", min_value=0, max_value=10000, value=5000)
+    median_income_slider = st.slider("Median Income", min_value=0, max_value=100000, value=50000)
+    poverty_rate_slider = st.slider("Poverty Rate", min_value=0, max_value=100, value=10)
+    time_of_commute_slider = st.slider("Time of Commute (minutes)", min_value=0, max_value=120, value=30)
 
 # User inputs
-user_input = [total_population_slider, median_income_slider, poverty_rate_slider, time_of_commute_slider]
+    user_input = [total_population_slider, median_income_slider, poverty_rate_slider, time_of_commute_slider]
 
 # Add an "Update" button to trigger predictions
-if st.button("Update Model"):
-    # Make predictions
-    prediction = make_predictions(knn_model, scaler, user_input)
-    st.write(f"Updated Prediction: {prediction}")
+    if st.button("Update Model"):
+        # Make predictions
+        prediction = make_predictions(knn_model, scaler, user_input)
+        st.write(f"Updated Prediction: {prediction}")
 
     # Plotting the data using Plotly Express with user customization
     st.header('Commuting Pattern Visualization')
@@ -99,7 +99,7 @@ if st.button("Update Model"):
     size_variable = st.selectbox("Select Size Variable", ['Total Population'])
     color_variable = st.selectbox("Select Y-Axis Variable", ['Poverty Rate'])
 
-    # Flip the color axis and y-axis variable
+    #Tract visual
     fig = px.scatter(df,
         x='Median Income',
         y=y_variable,  
