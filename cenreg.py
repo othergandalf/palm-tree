8088d5b1aab4a644cf6ddd86c0f6988c11330799
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-08 16:36:33 -0500

    Update cenreg.py

--- cenreg.py
+++ cenreg.py
@@ -5,9 +5,9 @@ from us import states
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
+import plotly.express as px
 
 def fetch_census_data():
-    # Fetch census data for all MI tracts
     c = Census("2cad02e99c0bde70c790f7391ffb3363c5e426ef")
     fields = [
         'NAME', 'B08301_001E', 'B08301_002E', 'B08301_003E', 'B08301_008E',
@@ -15,6 +15,7 @@ def fetch_census_data():
         'B01003_001E', 'B19101_001E', 'B17001_002E', 'B08303_001E'
     ]
 
+    # #Fetch census data for all MI tracts
     census_data = c.acs5.state_county_tract(
         fields=fields,
         county_fips="*",
@@ -42,30 +43,22 @@ def fetch_census_data():
 
     return df
 
-def train_knn_model(df):
-    commute_variables = ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home']
-    knn_models = {}
-    scalers = {}
+def train_knn_model(df, y_variable):
+    selected_features = ['Total Population', 'Median Income', 'Poverty Rate', 'Time of Commute']
+    X = df[selected_features]
+    y = df[y_variable]
 
-    for y_variable in commute_variables:
-        selected_features = ['Total Population', 'Median Income', 'Poverty Rate', 'Time of Commute']
-        X = df[selected_features]
-        y = df[y_variable]
+    imputer = SimpleImputer(strategy='median')
+    X_imp = imputer.fit_transform(X)
+    df[selected_features] = X_imp
 
-        imputer = SimpleImputer(strategy='median')
-        X_imp = imputer.fit_transform(X)
-        df[selected_features] = X_imp
+    scaler = StandardScaler()
+    scaled_X = scaler.fit_transform(X_imp)
 
-        scaler = StandardScaler()
-        scaled_X = scaler.fit_transform(X_imp)
+    knn_model = KNeighborsClassifier(n_neighbors=3)
+    knn_model.fit(scaled_X, y)
 
-        knn_model = KNeighborsClassifier(n_neighbors=3)
-        knn_model.fit(scaled_X, y)
-
-        knn_models[y_variable] = knn_model
-        scalers[y_variable] = scaler
-
-    return knn_models, scalers
+    return knn_model, scaler
 
 def make_predictions(knn_model, scaler, user_input):
     # Scale user input to match the scale of the training data
@@ -83,23 +76,44 @@ def show():
     # KNN model training
     st.header('KNN Model Training')
 
-    # Initialize user inputs
+    # User selects the y-variable for the commute
+    y_variable = st.selectbox("Select Commute Variable for KNN Model", ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home'],
+                              key="0001")
+
+    # Train the KNN model and get the scaler based on user-selected y-variable
+    knn_model, scaler = train_knn_model(df, y_variable)
+
+    # Add widgets for user inputs with unique keys
     total_population_slider = st.slider("Total Population", key="total_population", min_value=0, max_value=10000, value=5000)
     median_income_slider = st.slider("Median Income", key="median_income", min_value=0, max_value=100000, value=50000)
     poverty_rate_slider = st.slider("Poverty Rate", key="poverty_rate", min_value=0, max_value=100, value=10)
     time_of_commute_slider = st.slider("Time of Commute (minutes)", key="time_of_commute", min_value=0, max_value=120, value=30)
 
-    # Train the KNN models and get the scalers
-    knn_models, scalers = train_knn_model(df)
+    # User inputs
+    user_input = [total_population_slider, median_income_slider, poverty_rate_slider, time_of_commute_slider]
 
-    # Display predictions for each commute variable
-    st.header('Predictions for Each Commute Variable')
+    # Make predictions
+    prediction = make_predictions(knn_model, scaler, user_input)
+    st.write(f"Updated Prediction ({y_variable}): {prediction}")
 
-    commute_variables = ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home']
-    for y_variable in commute_variables:
-        # Make predictions
-        prediction = make_predictions(knn_models[y_variable], scalers[y_variable], [total_population_slider, median_income_slider, poverty_rate_slider, time_of_commute_slider])
-        st.write(f"Prediction ({y_variable}): {prediction}")
+    # Plotting the data using Plotly Express with user customization
+    st.header('Commute Count at the Tract-Level')
+    color_variable = 'Poverty Rate'  # Assuming this as a default color variable
+    graph_y_variable = st.selectbox("Select Y-Axis Commute Variable in Scatterplot",
+                                     ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home'],
+                                     key="0002")
 
-if __name__ == "__main__":
-    show()
+    # Tract visual
+    fig = px.scatter(df,
+        x='Median Income',
+        y=graph_y_variable,
+        color=color_variable,
+        size='Total Population',
+        hover_data=['NAME']
+    )
+
+    st.plotly_chart(fig)
+
+show()
+
+show()
