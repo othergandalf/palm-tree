a6ec6912d4b492cccedc7c1c83080c402c7e21df
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-07 21:39:08 -0500

    Update cenreg.py

--- cenreg.py
+++ cenreg.py
@@ -78,37 +78,37 @@ def show():
     # Train the KNN model and get the scaler
     knn_model, scaler = train_knn_model(df)
 
-  # Add widgets for user inputs with unique keys
-total_population_slider = st.slider("Total Population", key="total_population", min_value=0, max_value=10000, value=5000)
-median_income_slider = st.slider("Median Income", key="median_income", min_value=0, max_value=100000, value=50000)
-poverty_rate_slider = st.slider("Poverty Rate", key="poverty_rate", min_value=0, max_value=100, value=10)
-time_of_commute_slider = st.slider("Time of Commute (minutes)", key="time_of_commute", min_value=0, max_value=120, value=30)
+      # Add widgets for user inputs with unique keys
+    total_population_slider = st.slider("Total Population", key="total_population", min_value=0, max_value=10000, value=5000)
+    median_income_slider = st.slider("Median Income", key="median_income", min_value=0, max_value=100000, value=50000)
+    poverty_rate_slider = st.slider("Poverty Rate", key="poverty_rate", min_value=0, max_value=100, value=10)
+    time_of_commute_slider = st.slider("Time of Commute (minutes)", key="time_of_commute", min_value=0, max_value=120, value=30)
 
-# User inputs
-user_input = [total_population_slider, median_income_slider, poverty_rate_slider, time_of_commute_slider]
+    # User inputs
+    user_input = [total_population_slider, median_income_slider, poverty_rate_slider, time_of_commute_slider]
 
-# Add an "Update" button to trigger predictions
-if st.button("Update Model"):
-    # Make predictions
-    prediction = make_predictions(knn_model, scaler, user_input)
-    st.write(f"Updated Prediction: {prediction}")
+    # Add an "Update" button to trigger predictions
+    if st.button("Update Model"):
+        # Make predictions
+        prediction = make_predictions(knn_model, scaler, user_input)
+        st.write(f"Updated Prediction: {prediction}")
 
-# Plotting the data using Plotly Express with user customization
-st.header('Commuting Pattern Visualization')
-y_variable = st.selectbox("Select Color Variable", ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home'])
-size_variable = st.selectbox("Select Size Variable", ['Total Population'])
-color_variable = st.selectbox("Select Y-Axis Variable", ['Poverty Rate'])
+    # Plotting the data using Plotly Express with user customization
+    st.header('Commuting Pattern Visualization')
+    y_variable = st.selectbox("Select Color Variable", ['Driving Alone', 'Carpooling', 'Public Transportation', 'Walking', 'Cycling', 'Other Means', 'Worked from Home'])
+    size_variable = st.selectbox("Select Size Variable", ['Total Population'])
+    color_variable = st.selectbox("Select Y-Axis Variable", ['Poverty Rate'])
 
-# Tract visual
-fig = px.scatter(df,
-    x='Median Income',
-    y=y_variable,  
-    color=color_variable,  
-    size=size_variable,
-    hover_data=['NAME']
+    # Tract visual
+    fig = px.scatter(df,
+        x='Median Income',
+        y=y_variable,  
+        color=color_variable,  
+        size=size_variable,
+        hover_data=['NAME']
 )
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig)
 
 show()
 
