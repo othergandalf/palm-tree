98c8ba3ef09c73d7ebacc4be0c4ccdad6e1adbd5
othergandalf <145704671+othergandalf@users.noreply.github.com>
2023-12-08 15:53:15 -0500

    Update cenreg.py

--- cenreg.py
+++ cenreg.py
@@ -44,7 +44,7 @@ def fetch_census_data():
 
     return df
 
-    @st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def train_knn_model(df, y_variable):
     selected_features = ['Total Population', 'Median Income', 'Poverty Rate', 'Time of Commute']
     X = df[selected_features]
